114f472ab1535728117ad747bdba89f17e4f7298
Benedikt Waldvogel <benedikt.waldvogel@cronn.de>
2016-10-13 09:59:08 +0200

    remove unused code

--- src/main/java/de/cronn/jira/sync/domain/JiraLoginResponse.java
+++ src/main/java/de/cronn/jira/sync/domain/JiraLoginResponse.java
@@ -3,7 +3,6 @@ package de.cronn.jira.sync.domain;
 public class JiraLoginResponse {
 
 	private JiraSession session;
-	private LoginInfo loginInfo;
 
 	public JiraSession getSession() {
 		return session;
@@ -11,54 +10,6 @@ public class JiraLoginResponse {
 
 	public void setSession(JiraSession session) {
 		this.session = session;
-	}
-
-	public LoginInfo getLoginInfo() {
-		return loginInfo;
-	}
-
-	public void setLoginInfo(LoginInfo loginInfo) {
-		this.loginInfo = loginInfo;
-	}
-
-	public static class LoginInfo {
-
-		private int failedLoginCount;
-		private int loginCount;
-		private String lastFailedLoginTime;
-		private String previousLoginTime;
-
-		public int getFailedLoginCount() {
-			return failedLoginCount;
-		}
-
-		public void setFailedLoginCount(int failedLoginCount) {
-			this.failedLoginCount = failedLoginCount;
-		}
-
-		public int getLoginCount() {
-			return loginCount;
-		}
-
-		public void setLoginCount(int loginCount) {
-			this.loginCount = loginCount;
-		}
-
-		public String getLastFailedLoginTime() {
-			return lastFailedLoginTime;
-		}
-
-		public void setLastFailedLoginTime(String lastFailedLoginTime) {
-			this.lastFailedLoginTime = lastFailedLoginTime;
-		}
-
-		public String getPreviousLoginTime() {
-			return previousLoginTime;
-		}
-
-		public void setPreviousLoginTime(String previousLoginTime) {
-			this.previousLoginTime = previousLoginTime;
-		}
 	}
 
 }
