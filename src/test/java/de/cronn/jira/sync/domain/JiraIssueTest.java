75b734f1e1c7ee0f6025bf9f7d5b0940ba06f25a
Benedikt Waldvogel <benedikt.waldvogel@cronn.de>
2016-10-16 18:52:12 +0200

    use ISO8601 for JSON date formatting

--- src/test/java/de/cronn/jira/sync/domain/JiraIssueTest.java
+++ src/test/java/de/cronn/jira/sync/domain/JiraIssueTest.java
@@ -1,9 +1,11 @@
 package de.cronn.jira.sync.domain;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 public class JiraIssueTest {
 
@@ -11,6 +13,11 @@ public class JiraIssueTest {
 	public void testToString() {
 		JiraIssue issue = new JiraIssue("1", "KEY-123");
 		assertThat(issue.toString(), is("JiraIssue[id=1,key=KEY-123]"));
+	}
+
+	@Test
+	public void updatedTimestamp() throws Exception {
+		new ISO8601DateFormat().parse("2016-10-13T09:21:13.000+0200");
 	}
 
 }
