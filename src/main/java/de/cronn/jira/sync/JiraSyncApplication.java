28015ffe00840450e820114d44d8a6781d6a7050
Benedikt Waldvogel <benedikt.waldvogel@cronn.de>
2016-10-17 09:30:19 +0200

    configure jackson in Java

--- src/main/java/de/cronn/jira/sync/JiraSyncApplication.java
+++ src/main/java/de/cronn/jira/sync/JiraSyncApplication.java
@@ -2,11 +2,24 @@ package de.cronn.jira.sync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SpringBootApplication
 @EnableCaching
 public class JiraSyncApplication {
+
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer mapperBuilderCustomizer() {
+		return mapperBuilder -> {
+			mapperBuilder.serializationInclusion(Include.NON_NULL);
+			mapperBuilder.featuresToEnable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JiraSyncApplication.class, args);
