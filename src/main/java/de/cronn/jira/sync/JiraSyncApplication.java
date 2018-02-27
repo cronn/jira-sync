b678e454ccc54cee02c7707551a9777477126b9f
Benedikt Waldvogel <benedikt.waldvogel@cronn.de>
2018-02-27 13:59:29 +0100

    log warning when default charset is not UTF-8

--- src/main/java/de/cronn/jira/sync/JiraSyncApplication.java
+++ src/main/java/de/cronn/jira/sync/JiraSyncApplication.java
@@ -1,5 +1,12 @@
 package de.cronn.jira.sync;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -13,12 +20,23 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 @EnableCaching
 public class JiraSyncApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(JiraSyncApplication.class);
+
 	@Bean
 	public Jackson2ObjectMapperBuilderCustomizer mapperBuilderCustomizer() {
 		return mapperBuilder -> {
 			mapperBuilder.serializationInclusion(Include.NON_NULL);
 			mapperBuilder.featuresToEnable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		};
+	}
+
+	@PostConstruct
+	public void checkFileEncoding() {
+		Charset defaultCharset = Charset.defaultCharset();
+		Charset expectedCharset = StandardCharsets.UTF_8;
+		if (!defaultCharset.equals(expectedCharset)) {
+			log.warn("Default charset is '{}'. Consider switching environment to '{}'", defaultCharset, expectedCharset);
+		}
 	}
 
 	public static void main(String[] args) {
